cdc5c7636eb2614f3ba2893dc50afe7f2222c938
tejpal singh <tej@dhwaniastro.com>
2026-09-11 14:23:28 +0530

    change for logs

--- src/server.js
+++ src/server.js
@@ -20,7 +20,7 @@ async function startServer() {
 
   // ================= MIDDLEWARE =================
 app.use(cors({
-    origin: ["https://astropanel.dhwaniastro.com/", "https://astro-panel-satge-git-main-tej-engs-projects.vercel.app","https://astro-panel-v2.vercel.app"],
+    origin: ["https://astropanel.dhwaniastro.com", "https://astro-panel-satge-git-main-tej-engs-projects.vercel.app","https://astro-panel-v2.vercel.app"],
   credentials: true,
 }));
   app.use(express.json());
